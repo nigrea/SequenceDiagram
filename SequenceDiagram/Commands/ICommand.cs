b7b5f4be21399a6c304930e62080077306a1bb57
Nigrea <nigrea@gmail.com>
2014-10-21 11:18:20 +0200

    All the things

---
+++ SequenceDiagram/Commands/ICommand.cs
@@ -0,0 +1,16 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsProgrammingAssignment.Command
+{
+    interface ICommand
+    {
+        void run();
+        void undo();
+           
+    }
+}
