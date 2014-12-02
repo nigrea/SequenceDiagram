bfe1b79e0cfeea189d977939ab3eb38d6267e6c1
Nigrea <nigrea@gmail.com>
2014-12-02 11:11:56 +0100

    working on messages

--- Elements/Message.cs
+++ Elements/Message.cs
@@ -16,12 +16,21 @@ namespace Elements
         private int y;
         public int Y { get { return y; } set { y = value; NotifyPropertyChanged("Y"); } }
 
+        public int YTop { get { return y - 20; } set { YTop = value; NotifyPropertyChanged("YTop"); } }
+        public int YBot { get { return y + 20; } set { YBot = value; NotifyPropertyChanged("YBot"); } }
+        public int XLeft { get { if (start.Position < end.Position) { return end.CanvasCenterX - 20; } else { return end.CanvasCenterX + 20; }; } set { if (start.Position < end.Position) { end.CanvasCenterX = value + 20; } else { end.CanvasCenterX = value - 20;  }; NotifyPropertyChanged("XLeft"); } }
+
+        private string name;
+        public string Name { get { return name; } set { name = value; NotifyPropertyChanged("Name"); } }
+
+
         private Component start;
         private Component end;
         public Component Start { get { return start; } set { start = value; NotifyPropertyChanged("EndA"); } }
         public Component End { get { return end; } set { end = value; NotifyPropertyChanged("EndB"); } }
 
         public Message(Component start, Component end){
+            this.name = "Function Name";
             this.start = start;
             this.end = end;
         }
